5963a55ce42b15468f07ea2b770ac7e3479a372e
skippy <rcthedev@gmail.com>
2020-05-10 18:15:35 -0700

    trying heroku again

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,19 @@
 // set up mysql connection
 let mysql = require('mysql');
 
-
-    let connection = mysql.createConnection({
+let connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+        connection = mysql.createConnection({
         host: 'localhost',
         port: 3306,
         user: 'root',
         password: 'Sage43236984C!',
         database: 'burgerland_db'
     });
-
+};
 
 // make connection
 connection.connect(function(err) {
